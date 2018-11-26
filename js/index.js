908b76f3e74dffbb297cde28bc2340f382bf0bb8
FX Wood <fxwood2@gmail.com>
2018-11-26 05:05:32 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -18,10 +18,10 @@ input.addEventListener('keydown', function(e) {
 
 let divs = document.querySelectorAll('div');
 
-function bubble(e)  {
+function capture(e)  {
   console.log(this.firstChild.nodeValue.trim() + ' bubbled')
 }
 
 for (let i = 0; i < divs.length; i++) {
-  divs[i].addEventListener('click', function(e) {}, true);
+  divs[i].addEventListener('click', capture, true);
 }
